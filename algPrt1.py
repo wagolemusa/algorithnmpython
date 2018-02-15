2da2f3160dfcfff12f5c48e587d8feaa01a3af54
kisumutalent <homiemusa@gmail.com>
2018-02-15 10:53:11 +0300

    Data Structures and Algorithms using Python Week 1 Lec -1 | NPTEL

---
+++ algPrt1.py
@@ -0,0 +1,21 @@
+#use fm, fn, for list of factors of m, n, respectively
+#for each i from 1 to m, add i to fm if i divides m
+#for each j from 1 to n, add j to fn if j divides n
+#use cf for list of common factors
+#for each f in fm, add f to cf if also appears in fn
+#Return largest(rightmost(values in cf
+
+def gcd(m,n):
+     fm = []
+     for i in range(1,m+1):
+          if (m%1) == 0:
+               fm.append(i)
+     fn = []
+     for j in range(1,n+1):
+          if (n%j) == 0:
+               fn.append(j)
+     cf = []
+     for f in fm:
+          if f in fn:
+               cf.append(f)
+     return(cf[-1])
