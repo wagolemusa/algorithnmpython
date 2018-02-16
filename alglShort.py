d64d82879e3b4d0d88dc846adfa5a215dd0e5a6f
kisumutalent <homiemusa@gmail.com>
2018-02-16 11:58:38 +0300

    different ways of solving Algorithms

--- alglShort.py
+++ alglShort.py
@@ -1,6 +1,4 @@
 # short way
-
-
 def gcd(m,n):
 
      cf = []
@@ -8,3 +6,25 @@ def gcd(m,n):
           if (m%i) == 0 and (n%i) == 0:
                cf.append(i)
      return(cf[-1])
+
+
+#When There is No lists!
+
+def gcd(m,n):
+     for i in range(1,min(m,n)+1):
+          if (m%i) == 0 and(n%i) == 0:
+               mrcf = i
+     return (mrcf)
+
+
+#No lists using while
+
+def gcd(m,n):
+
+     i = min(m,n)
+
+     while i > 0:
+          if (m%i) == 0 and (n%i) == 0:
+               return(i) #----> exit
+          else:
+               i = i-1 #update i to i-1
