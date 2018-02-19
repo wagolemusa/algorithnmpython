e07a9a1c7633189af5a6dbb28421a85228a2d307
kisumutalent <homiemusa@gmail.com>
2018-02-19 20:05:50 +0300

    simple way of removing words

--- andelaremove.py
+++ andelaremove.py
@@ -1,7 +1,7 @@
 def remove_deplicates(word):
      new_string = ' '.join(sorted(set(word)))
-     #removed_len = len(word) - len(new_string)
-     return new_string# removed_len
+     return new_string
 
 n = "refuge wise"
+
 print remove_deplicates(n)
